1011b79e0d794080ca294097108b9bfcec75c28c
Roberto Molowny-Horas <roberto@creaf.uab.es>
2024-11-07 16:02:49 +0100

    Added an internal sys.data.rda dataset.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,8 +4,56 @@ muni <- read.table(".\\data-raw\\MUNICIPIOS.csv", sep = ";", dec = ",", header =
                 quote = "\"", colClasses = c("COD_GEO" = "character"))
 
 # Instituto Nacional de Estadística (INE) database (https://www.ine.es/daco/daco42/codmun/diccionario24.xlsx).
-dicc <- readxl::read_excel(".\\data-raw\\diccionario24.xlsx", skip = 1, col_names = T)
+dicc <- readxl::read_excel(".\\data-raw\\diccionario24.xlsx", skip = 1, col_names = T) |>
+  as.data.frame()
+prov <- readxl::read_excel(".\\data-raw\\Comunidades Autónomas y provincias.xlsx", col_names = T) |>  as.data.frame()
 
-# Substitute curly quotation marks, if any.
-muni$NOMBRE_ACTUAL_nocurly <- dendrohist::curly_quotes(muni$NOMBRE_ACTUAL, F)
-dicc$NOMBRE_nocurly <- dendrohist::curly_quotes(dicc$NOMBRE, F)
+
+
+
+#' Preprocess datasets.
+#'
+#' @description
+#' Little function to:
+#' - Substitute curly quotation marks.
+#' - Replace diacritics and 'ñ'.
+#' - Switch to lower case letters.
+#' - Split string by "/" and keep the left part.
+#' - Swap words divided by a comma.
+#' - Remove white space.
+#'
+#' @param df \code{data.frame} containing data fro INE or CNIG databases.
+#' @param colnam \code{character} string with the name of the column to process.
+#'
+#' @return
+#' The input \code{data.frame} with a modified 'colnam' column.
+#'
+preprocess <- function(df, colnam) {
+  df[, colnam] <- df[, colnam] |> dendrohist::curly_quotes(FALSE) |>
+    dendrohist::replace_accent() |>
+    tolower() |>
+    dendrohist::string_split("/") |>
+    dplyr::select(left) |>
+    dendrohist::comma_swap() |>
+    gsub("' ", "'", .)
+  return(df)
+}
+
+
+# Process columns.
+muni <- f1(muni, "NOMBRE_ACTUAL")
+dicc <- f1(dicc, "NOMBRE")
+
+
+# Municipal codes.
+i <- match(muni$NOMBRE_ACTUAL, dicc$NOMBRE)
+muni <- cbind(muni, dicc[i, ] |> dplyr::select(CODAUTO, CPRO, CMUN, DC))
+muni$PO <- paste0(dicc$CPRO[i], dicc$CMUN[i])
+
+
+# Save 'muni'.
+save(muni, file = "R/sysdata.rda")
+
+
+
+
